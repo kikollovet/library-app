a1afa9e3e504dd000ce90c08b5ad34fa2d0f5d1f
Kiko <kikollovet@gmail.com>
2020-11-11 11:33:08 -0300

    identation reviewed

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,10 +2,9 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import firebase from 'firebase/app';
 
-export default Controller.extend ({
+export default Controller.extend({
 
     session: service(),
-    
     loggedUser: service(),
 
 })
--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,26 +3,26 @@ import { inject as service } from '@ember/service';
 import firebase from 'firebase/app';
 
 export default Controller.extend({
-    
+
     session: service(),
     firebaseApp: service(),
-      
+
     actions: {
         async login() {
             const auth = await this.get('firebaseApp').auth();
 
             const email = this.get('email');
             const password = this.get('password');
-            
+
             return auth.signInWithEmailAndPassword(email, password).catch((error) => {
 
-                 var errorCode = error.code;
-                 var errorMessage = error.message;
-                 console.log(errorCode, errorMessage);
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
 
                 this.set('errorLogin', errorMessage)
 
-               })
-            }
+            })
         }
+    }
 });
