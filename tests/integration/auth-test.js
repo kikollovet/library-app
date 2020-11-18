1293ace654334c87ed1c7c752f95ecadfcf4fad3
Kiko <kikollovet@gmail.com>
2020-11-18 20:29:23 -0300

    Added test that saves invitation in ember local storage

--- tests/integration/auth-test.js
+++ tests/integration/auth-test.js
@@ -1,7 +1,6 @@
 import { module, test } from 'qunit';
 import { visit, currentURL, click, fillIn } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-//import JSONAPIAdapter from '@ember-data/adapter/json-api';
 
 module('Acceptance | login unauthorized', function(hooks) {
   setupApplicationTest(hooks);
@@ -20,13 +19,4 @@ module('Acceptance | login unauthorized', function(hooks) {
     await visit('admin/seeder');
     assert.equal(currentURL(), '/login');
   });
-
-  // test('Saving Invitation', async function(assert) {
-  //  // this.owner.register('adapter:application', class TestAdapter extends JSONAPIAdapter {
-  //  // });
-  //   await visit('');
-  //   await fillIn('input#emailField', 'insertionfromTHETEST@teste.com');
-  //   await click('button#submitButton');
-  //   assert.equal(currentURL(), '');
-  // });
 });
--- tests/integration/auth2-test.js
+++ tests/integration/auth2-test.js
@@ -1,35 +1,59 @@
 import { module, test } from 'qunit';
 import { visit, currentURL, click, fillIn, pauseTest, find, findAll } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-import $ from 'jquery';
-import { currentSession } from 'ember-simple-auth/test-support';
-import { inject as service } from '@ember/service';
+import { currentSession, authenticateSession, invalidateSession } from 'ember-simple-auth/test-support';
 import { tracked } from '@glimmer/tracking';
 import Service from '@ember/service';
+import Adapter from 'ember-local-storage/adapters/local';
+import resetStorages from 'ember-local-storage/test-support/reset-storage';
 
 module('Acceptance | login Authentificated', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting admin invitations with proper authorization', async function(assert) {
+  hooks.afterEach(function() {
+    if (window.localStorage) {
+      window.localStorage.clear();
+    }
+    if (window.sessionStorage) {
+      window.sessionStorage.clear();
+    }
+    resetStorages();
+  });
+
+  test('sending an invitation, visiting admin invitations with authorization, confirming invitation sended', async function(assert) {
 
     this.owner.register('service:logged-user', class TestService extends Service {
-      @tracked user = {email: 'ola@test.com'}
+      @tracked user = {email: 'user@teste.com'}
     });
 
+    this.owner.register('adapter:application', class TestService extends Adapter {
+   
+    });
 
-    
-    // await visit('admin/invitations');
+    await visit('');
+      await fillIn('input#emailField', 'cool@cool.com');
+      await click('button#submitButton');
+
+    await visit('admin/invitations');
     // await fillIn('input#emailField', 'user@teste.com');
     // await fillIn('input#passwordField', 'testeteste');
-    
-    currentSession(this.application).isAuthenticated = true;
-    currentSession(this.application).sessionDataUpdated;
     // await click('button#submitButton');
-  
-    await visit('admin/invitations');
+
+    // currentSession(this.application).isAuthenticated = true;
+    // currentSession(this.application).sessionDataUpdated;
+    
+    await authenticateSession();
     await pauseTest();
-    assert.equal(currentURL(), 'admin/invitations');
+    
+    assert.equal(currentURL(), '/admin/invitations');
     assert.equal(this.element.querySelector('h1').textContent, 'Invitations');
-    //assert.equal(find('.invitationEmail').textContent, 'user@teste.com');
+    assert.equal(find('.invitationEmail').textContent, 'cool@cool.com');
+    
+    //await click('button#logoutButton');
+    await invalidateSession();
+    
+    await visit('admin/contacts');
+    await pauseTest();
+    assert.equal(currentURL(), '/login');
   });
 });
